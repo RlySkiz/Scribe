2200bc6e66500da41554604b347e83a858ed2145
Aahzmandius <5201917+Aahzmandius@users.noreply.github.com>
2025-02-13 15:05:29 -0500

    Add color animation and interpolation functions to Imgui and ported over some more things to Helpers.Color

--- Mods/Scribe/ScriptExtender/Lua/Lib/Aahz/Client/Classes/ImguiAnimation.lua
+++ Mods/Scribe/ScriptExtender/Lua/Lib/Aahz/Client/Classes/ImguiAnimation.lua
@@ -1,6 +1,59 @@
 RX = RX or require("Lib.ReactiveX.reactivex._init")
 Imgui = Imgui or {}
 
+-- Internal animation tick setup
+Imgui._mainScheduler = RX.CooperativeScheduler.Create()
+Imgui.MainTick = RX.Subject.FromCoroutine(function()
+    local i = 1
+    while true do
+        _P(i)
+        coroutine.yield(i)
+        if i >= 60 then i = 0 else i = i + 1 end
+    end
+end, Imgui._mainScheduler)
+local fps = 1/60
+Ext.Timer.WaitForRealtime(fps, function() Imgui._mainScheduler:Update(fps*.001) end, fps)
+
+---Animates an imgui element's GuiColor colorProp between an initial and highlight color for a short time
+---@param el ExtuiStyledRenderable
+---@param colorProp GuiColor
+---@param initialColor vec4
+---@param highlightColor vec4
+---@param animationLength integer # length in frames, ideally multiples of 60 for smooth tweening, since internal tick is 60 fps
+function Imgui.AnimateColor(el, colorProp, initialColor, highlightColor, animationLength)
+    local framesPassed = 0
+
+    -- based on frame number (1-60), lerp between initial color and highlight color
+    local function simpleTween(frame)
+        framesPassed = framesPassed + 1
+
+        -- adjust frame - 1 so initial sine input results at 0
+        local adjustedFrame = frame - 1
+        -- it's a waaaaave, oscillate percent between 0 and 100 twice per second
+        local percent = (math.sin(adjustedFrame * math.pi / 30 - math.pi / 2) + 1) * 50
+
+        el:SetColor(colorProp, Helpers.Color.NormalizedLerp(initialColor, highlightColor, percent))
+    end
+    local function resetColor() el:SetColor(colorProp, initialColor) _P(string.format("Reset to: <%s, %s, %s, %s>", table.unpack(initialColor))) end
+    Imgui.MainTick:Take(animationLength):Subscribe(simpleTween, resetColor, resetColor)
+end
+
+-- local testWin
+-- -- Test animation only
+-- Ext.Events.ResetCompleted:Subscribe(function()
+--     testWin = Imgui.CreateCommonWindow("Testing Color Animation")
+--     testWin.Open = true
+--     Ext.OnNextTick(function()
+--         testWin:SetColor("WindowBg", Imgui.Colors.Blue)
+--     end)
+-- end)
+-- Ext.RegisterConsoleCommand("blue", function()
+--     Imgui.AnimateColor(testWin, "WindowBg", Imgui.Colors.Blue, Imgui.Colors.White, 120)
+-- end)
+-- Ext.RegisterConsoleCommand("green", function()
+--     Imgui.AnimateColor(testWin, "WindowBg", Imgui.Colors.MediumSeaGreen, Imgui.Colors.White, 120)
+-- end)
+
 ---@param el ExtuiTreeParent
 ---@param iconName string # name defined in atlas
 ---@param displaySize vec2 # ie- {64, 64} size of imgui image to display
--- Mods/Scribe/ScriptExtender/Lua/Lib/Aahz/Shared/Helpers/Color.lua
+++ Mods/Scribe/ScriptExtender/Lua/Lib/Aahz/Shared/Helpers/Color.lua
@@ -26,6 +26,36 @@ function Helpers.Color.HexToRGBA(hex, alpha)
 
     return {r,g,b,alpha}
 end
+
+---@param hex string
+---@return vec3
+function Helpers.Color.HexToRGB(hex)
+    hex = hex:gsub('#','')
+    local r,g,b
+
+	if hex:len() == 3 then
+		r = tonumber('0x'..hex:sub(1,1)) * 17
+        g = tonumber('0x'..hex:sub(2,2)) * 17
+        b = tonumber('0x'..hex:sub(3,3)) * 17
+	elseif hex:len() == 6 then
+		r = tonumber('0x'..hex:sub(1,2))
+        g = tonumber('0x'..hex:sub(3,4))
+        b = tonumber('0x'..hex:sub(5,6))
+    end
+
+    r = r or 0
+    g = g or 0
+    b = b or 0
+
+    return {r,g,b}
+end
+
+---@param rgb vec3
+---@return string
+function Helpers.Color.RGBToHex(rgb)
+    return string.format('%.2x%.2x%.2x', rgb[1], rgb[2], rgb[3])
+end
+
 --- Create a table for the RGBA values, normalized to 0-1
 --- This is useful because of syntax highlighting that is not present when typing a table directly
 ---@param r number
@@ -53,4 +83,48 @@ function Helpers.Color.NormalizedRGBToHex(r, g, b)
     g = math.floor(g * 255)
     b = math.floor(b * 255)
     return string.format("#%02x%02x%02x", r, g, b)
+end
+
+---Returns a linearly interpolated color between two hex colors
+---@param hex1 string
+---@param hex2 string
+---@param percent number 0-100
+---@return string hex value between "000000" and "FFFFFF"
+function Helpers.Color.LerpHex(hex1, hex2, percent)
+    percent = Ext.Math.Clamp(percent, 0, 100)
+    local rgb = Helpers.Color.HexToRGB(hex1)
+    local rgb2 = Helpers.Color.HexToRGB(hex2)
+    local rgb3 = {
+        math.floor((rgb[1]*(100-percent)/100) + (rgb2[1]*percent/100)),
+        math.floor((rgb[2]*(100-percent)/100) + (rgb2[2]*percent/100)),
+        math.floor((rgb[3]*(100-percent)/100) + (rgb2[3]*percent/100)),
+    }
+    return Helpers.Color.RGBToHex(rgb3)
+end
+---Returns a linearly interpolated color between two RGB/RGBA colors
+---@param color1 vec3|vec4
+---@param color2 vec3|vec4
+---@param percent number 0-100
+---@return vec3|vec4 color lerped rgb vec3 or rgba vec4
+function Helpers.Color.NormalizedLerp(color1, color2, percent)
+    if #color1 == 3 then
+        -- assume rgb
+        percent = Ext.Math.Clamp(percent, 0, 100)
+        local rgb3 = {
+            (color1[1]*(100-percent)/100) + (color2[1]*percent/100),
+            (color1[2]*(100-percent)/100) + (color2[2]*percent/100),
+            (color1[3]*(100-percent)/100) + (color2[3]*percent/100),
+        }
+        return rgb3
+    else
+        -- assume rgba
+        percent = Ext.Math.Clamp(percent, 0, 100)
+        local rgba4 = {
+            (color1[1]*(100-percent)/100) + (color2[1]*percent/100),
+            (color1[2]*(100-percent)/100) + (color2[2]*percent/100),
+            (color1[3]*(100-percent)/100) + (color2[3]*percent/100),
+            (color1[4]*(100-percent)/100) + (color2[4]*percent/100),
+        }
+        return rgba4
+    end
 end
